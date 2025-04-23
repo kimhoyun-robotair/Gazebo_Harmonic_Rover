3ba8b3c186ddd2028ec7d5463f453369d1dd04fd
kimhoyun-robotair <suberkut76@gmail.com>
2025-04-23 22:50:04 +0900

    Add sensors plugin

--- src/simple_rover/launch/spawn_robot.launch.py
+++ src/simple_rover/launch/spawn_robot.launch.py
@@ -16,7 +16,12 @@ def generate_launch_description():
 
     rviz_launch_arg = DeclareLaunchArgument(
         'rviz', default_value='true',
-        description='Open RViz.'
+        description='Open RViz'
+    )
+
+    rviz_config_arg = DeclareLaunchArgument(
+        'rviz_config', default_value='rviz.rviz',
+        description='RViz config file'
     )
 
     world_arg = DeclareLaunchArgument(
@@ -27,6 +32,26 @@ def generate_launch_description():
     model_arg = DeclareLaunchArgument(
         'model', default_value='simple_rover.urdf',
         description='Name of the URDF description to load'
+    )
+
+    x_arg = DeclareLaunchArgument(
+        'x', default_value='2.5',
+        description='x coordinate of spawned robot'
+    )
+
+    y_arg = DeclareLaunchArgument(
+        'y', default_value='1.5',
+        description='y coordinate of spawned robot'
+    )
+
+    yaw_arg = DeclareLaunchArgument(
+        'yaw', default_value='-1.5707',
+        description='yaw angle of spawned robot'
+    )
+
+    sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time', default_value='True',
+        description='Flag to enable use_sim_time'
     )
 
     # Define the path to your URDF or Xacro file
@@ -49,10 +74,10 @@ def generate_launch_description():
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
-        arguments=['-d', os.path.join(pkg_simple_rover, 'rviz', 'rviz.rviz')],
+        arguments=['-d', PathJoinSubstitution([pkg_simple_rover, 'rviz', LaunchConfiguration('rviz_config')])],
         condition=IfCondition(LaunchConfiguration('rviz')),
         parameters=[
-            {'use_sim_time': True},
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
         ]
     )
 
@@ -61,13 +86,64 @@ def generate_launch_description():
         package="ros_gz_sim",
         executable="create",
         arguments=[
-            "-name", "my_robot",
+            "-name", "mogi_bot",
             "-topic", "robot_description",
-            "-x", "0.0", "-y", "0.0", "-z", "0.5", "-Y", "0.0"  # Initial spawn position
+            "-x", LaunchConfiguration('x'), "-y", LaunchConfiguration('y'), "-z", "0.2", "-Y", LaunchConfiguration('yaw')  # Initial spawn position
         ],
         output="screen",
         parameters=[
-            {'use_sim_time': True},
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
+    )
+
+    # Node to bridge /cmd_vel and /odom
+    gz_bridge_node = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist",
+            "/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry",
+            "/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model",
+            "/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V",
+            #"/camera/image@sensor_msgs/msg/Image@gz.msgs.Image",
+            #"/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo",
+            # "/imu@sensor_msgs/msg/Imu@gz.msgs.IMU",
+            "/navsat@sensor_msgs/msg/NavSatFix@gz.msgs.NavSat",
+            #"/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan",
+            #"/scan/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked",
+            #"/camera/depth_image@sensor_msgs/msg/Image@gz.msgs.Image",
+            #"/camera/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked",
+        ],
+        output="screen",
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
+    )
+
+    # Node to bridge camera image with image_transport and compressed_image_transport
+    gz_image_bridge_node = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=[
+            "/camera/image",
+        ],
+        output="screen",
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time'),
+             'camera.image.compressed.jpeg_quality': 75},
+        ],
+    )
+
+    # Relay node to republish /camera/camera_info to /camera/image/camera_info
+    relay_camera_info_node = Node(
+        package='topic_tools',
+        executable='relay',
+        name='relay_camera_info',
+        output='screen',
+        arguments=['camera/camera_info', 'camera/image/camera_info'],
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
         ]
     )
 
@@ -78,7 +154,7 @@ def generate_launch_description():
         output='screen',
         parameters=[
             {'robot_description': Command(['xacro', ' ', urdf_file_path]),
-             'use_sim_time': True},
+             'use_sim_time': LaunchConfiguration('use_sim_time')},
         ],
         remappings=[
             ('/tf', 'tf'),
@@ -86,32 +162,22 @@ def generate_launch_description():
         ]
     )
 
-    # Node to bridge messages like /cmd_vel and /odom
-    gz_bridge_node = Node(
-        package="ros_gz_bridge",
-        executable="parameter_bridge",
-        arguments=[
-            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
-            "/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist",
-            "/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry",
-            "/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model",
-            "/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V"
-        ],
-        output="screen",
-        parameters=[
-            {'use_sim_time': True},
-        ]
-    )
-
     launchDescriptionObject = LaunchDescription()
 
     launchDescriptionObject.add_action(rviz_launch_arg)
+    launchDescriptionObject.add_action(rviz_config_arg)
     launchDescriptionObject.add_action(world_arg)
     launchDescriptionObject.add_action(model_arg)
+    launchDescriptionObject.add_action(x_arg)
+    launchDescriptionObject.add_action(y_arg)
+    launchDescriptionObject.add_action(yaw_arg)
+    launchDescriptionObject.add_action(sim_time_arg)
     launchDescriptionObject.add_action(world_launch)
     launchDescriptionObject.add_action(rviz_node)
     launchDescriptionObject.add_action(spawn_urdf_node)
-    launchDescriptionObject.add_action(robot_state_publisher_node)
     launchDescriptionObject.add_action(gz_bridge_node)
-    
+    launchDescriptionObject.add_action(gz_image_bridge_node)
+    launchDescriptionObject.add_action(relay_camera_info_node)
+    launchDescriptionObject.add_action(robot_state_publisher_node)
+
     return launchDescriptionObject
